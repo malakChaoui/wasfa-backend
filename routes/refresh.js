9503510977d9a491ee0db3ca61b44d1dafd3f6de
malak <m_chaoui@estin.dz>
2025-05-04 07:27:35 +0000

    changing refresh into a post

--- routes/refresh.js
+++ routes/refresh.js
@@ -1,5 +1,5 @@
 const express=require('express');
 const router=express.Router();
 const refresh=require('../controllers/refreshTokenController');
-router.get('/',refresh.handleRefreshToken);
+router.post('/',refresh.handleRefreshToken);
 module.exports=router;
